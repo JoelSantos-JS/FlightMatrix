eb260271318f6a86a0a99971431a6b31a387c3df
Joel Santos <joeltere9@gmail.com>
2025-04-21 10:33:31 -0300

    first commit

---
+++ src/main/java/com/joel/br/FlightMatrix/FlightMatrixApplication.java
@@ -0,0 +1,13 @@
+package com.joel.br.FlightMatrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightMatrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightMatrixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joel/br/FlightMatrix/FlightMatrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joel.br.FlightMatrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightMatrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
